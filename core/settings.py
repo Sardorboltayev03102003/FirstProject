3180cda36aa3a6efd9524146e7e16a7d4c9648cc
Sardorboltayev03102003 <boltayevs758@gmail.com>
2023-11-04 12:52:08 +0500

    salom

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-mh5elm*g8)&x=d!3=h53*3!+)sk9$nf6&$!^+vxpyd)laa8^z3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
